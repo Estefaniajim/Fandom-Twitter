64b48975a988a8b4b95353d2fafd7e43d0322475
Estefania Jimenez Garcia <estefyvocaloid@gmail.com>
2020-11-22 00:12:22 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,17 @@
+from dotenv import load_dotenv
 import os
+import tweepy
 
+load_dotenv()
 APIkey = os.getenv('APIkey')
 APIsecretkey = os.getenv('APIsecretkey')
-Bearertoken = os.getenv('Bearertoken')
+AccessToken = os.getenv("AccessToken")
+AccessTokenSecret = os.getenv("AccessTokenSecret")
+
+auth = tweepy.OAuthHandler(APIkey, APIsecretkey)
+auth.set_access_token(AccessToken, AccessTokenSecret)
+api = tweepy.API(auth)
+
+def dataUser():
+    userDic = {'name': api.me().name, 'photo': api.me().profile_image_url}
+    return userDic
