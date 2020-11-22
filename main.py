3e77b6da6519a2dc595395b0e2afe4b80dc26ecc
Juan Pablo Velazco Velasquez <juanpa.jpvv@gmail.com>
2020-11-22 00:44:10 -0600

    Profile picture working
    Signed-off-by: Juan Pablo Velazco Velasquez <juanpa.jpvv@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+import tweepy
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
+    load_dotenv()
+    APIkey = os.getenv('APIkey')
+    APIsecretkey = os.getenv('APIsecretkey')
+    AccessToken = os.getenv("AccessToken")
+    AccessTokenSecret = os.getenv("AccessTokenSecret")
+
+    auth = tweepy.OAuthHandler(APIkey, APIsecretkey)
+    auth.set_access_token(AccessToken, AccessTokenSecret)
+    api = tweepy.API(auth)
+    photo_url = api.me().profile_image_url
+    photo_url_bigger = photo_url[0:len(photo_url) - 11]
+    photo_url_bigger = photo_url_bigger + ".jpg"
     return render_template('index.html',
                            user={
-                               'name': 'Someone', 'photo': 'https://pbs.twimg.com/profile_images/1012717264108318722/9lP-d2yM_400x400.jpg'},
+                               'name': api.me().screen_name, 'photo': photo_url_bigger},
                            hashtags=[
-                               {'name': 'perros', 'url': '/fandom'},
+                               {'name': 'BTS', 'url': '/fandom'},
                                {'name': 'gatos', 'url': '/fandom'},
                                {'name': 'pandas', 'url': '/fandom'},
                            ],your_fandoms=[
@@ -22,9 +36,21 @@ def hello_world():
 @app.route('/fandom')
 @app.route('/fandom/<fandom_name>')
 def fandom(fandom_name="BTS"):
+    load_dotenv()
+    APIkey = os.getenv('APIkey')
+    APIsecretkey = os.getenv('APIsecretkey')
+    AccessToken = os.getenv("AccessToken")
+    AccessTokenSecret = os.getenv("AccessTokenSecret")
+
+    auth = tweepy.OAuthHandler(APIkey, APIsecretkey)
+    auth.set_access_token(AccessToken, AccessTokenSecret)
+    api = tweepy.API(auth)
+    photo_url = api.me().profile_image_url
+    photo_url_bigger = photo_url[0:len(photo_url) - 11]
+    photo_url_bigger = photo_url_bigger + ".jpg"
     return render_template('fandom.html', fandom_name=fandom_name,
                            user={
-                               'name': 'Someone', 'photo': 'https://pbs.twimg.com/profile_images/1012717264108318722/9lP-d2yM_400x400.jpg'},
+                               'name': api.me().screen_name, 'photo': photo_url_bigger},
                            your_fandoms=[
                                {'name': 'BTS', 'url': '#'},
                                {'name': 'Star Wars', 'url': '#'},
